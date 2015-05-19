67045514e3f1064b41b9b5a3d0fdcce80d7f8a08
dongyado <slaying.monsters@gmail.com>
2015-05-19 17:07:53 +0800

    list page updated

--- www/js/services.js
+++ www/js/services.js
@@ -29,8 +29,6 @@ angular.module('starter.services', [])
         });
     };
 
-
-
     return {
         getApiData: getApiData
     };
@@ -41,30 +39,30 @@ angular.module('starter.services', [])
     // Some fake testing data
     var chats = [{
         id: 0,
-name: '冬芽',
-lastText: 'You on your way?',
-face: 'http://wx.qlogo.cn/mmopen/gtGnxwNxfyJ1EORswEYbldianV76sOPrj9H85Wq8ZyPKmG3Tf5JwoiaFQ0Nib7dsMlI13pmVh9eWgGATKtZxGpVaauVuXQq98Ad/96'
-    }, {
-        id: 1,
-name: '哈哈',
-lastText: 'Hey, it\'s me',
-face: 'http://imgcdn.lerays.com/vhead/5243fbf2b2119313df7539b666380cd791238d86.jpg?f=c'
+        name: '冬芽',
+        lastText: 'You on your way?',
+        face: 'http://wx.qlogo.cn/mmopen/gtGnxwNxfyJ1EORswEYbldianV76sOPrj9H85Wq8ZyPKmG3Tf5JwoiaFQ0Nib7dsMlI13pmVh9eWgGATKtZxGpVaauVuXQq98Ad/96'
+            }, {
+                id: 1,
+        name: '哈哈',
+        lastText: 'Hey, it\'s me',
+        face: 'http://imgcdn.lerays.com/vhead/5243fbf2b2119313df7539b666380cd791238d86.jpg?f=c'
     }];
 
     return {
         all: function() {
             return chats;
         },
-remove: function(chat) {
-    chats.splice(chats.indexOf(chat), 1);
-},
-    get: function(chatId) {
-        for (var i = 0; i < chats.length; i++) {
-            if (chats[i].id === parseInt(chatId)) {
-                return chats[i];
+        remove: function(chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function(chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
             }
+            return null;
         }
-        return null;
-    }
-};
+    };
 });
